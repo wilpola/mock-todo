d29472a431b784ada064dc0c5a789f06a5b784e7
wilpola <wilpolaproduction@gmail.com>
2023-02-03 00:18:48 +0200

    Initializes Navigaiton component

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,22 @@
+// @author: wilpola
+// This file contains the navigaiton component
+
+// imported packages
+import { useState, useEffect} from 'react';
+import './navigation.scss';
+
+// Navigation component
+const Navigation = () => {
+
+    // All rendered content
+    return (
+        <nav id="Navigation">
+            <div className="nav-logo">
+
+            </div>
+            <div className="nav-links">
+
+            </div>
+        </nav>
+    )
+}
