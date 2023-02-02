eb2bfaa8012ed7daa7f3c49ff394f1cd3a1a9a17
wilpola <wilpolaproduction@gmail.com>
2023-02-03 00:12:31 +0200

    Initializes the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// Imported packages
+import { useState, useEffect } from "react";
 
+// Import styles
+import "./App.scss";
+
+// App component
 function App() {
+  // States
+  const [todos, setTodos] = useState([{}]);
+
+  // On first page load
+  useEffect(() => {
+    // creating a localStorage variables for ease of use
+    const lS = window.localStorage;
+
+    if (!lS.getItem("wilp_todo")) {
+      const mockData = [
+        {
+          title: "Clean your room",
+          status: "Not Started",
+          date: "",
+          dueDate: "",
+          notes: []
+        },
+        {
+          title: 'Get groceries',
+          status: 'Not started',
+          date: new Date(),
+          dueDate: '',
+          notes: ''
+        }
+      ];
+      lS.setItem("wilp_todo", JSON.stringify(mockData));
+    } else {
+      setTodos(JSON.parse(lS.getItem("wilp_todo")));
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Todo App</h1>
+      <div id="todo-container">
+        {todos.map((item, key) => {
+          return (
+            <div className="todoItem" key={key}>
+              <input type={"checkbox"} className='checkbox'/>
+              <p>{item.title}</p>{" "}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
