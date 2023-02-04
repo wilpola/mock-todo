5f0a335247c57b360eb37d7ec1858bd0aed5563f
wilpola <wilpolaproduction@gmail.com>
2023-02-04 21:31:08 +0200

    attempt to fix rebase

--- src/App.js
+++ src/App.js
@@ -6,13 +6,33 @@ import { Navigation, SearchBar } from "./components";
 
 // Import styles
 import "./App.scss";
-import { CompletedPage, TodayPage } from "./pages";
+import { CompletedPage, TodayPage, Settings } from "./pages";
 import { v4 as uuidv4 } from "uuid";
 
 // App component
 function App() {
   // States
   const [todos, setTodos] = useState([{}]);
+  const [theme, setTheme] = useState("light");
+
+  const mockData = [
+    {
+      id: uuidv4(),
+      title: "Clean your room",
+      status: "Not Started",
+      date: new Date().toLocaleDateString("en-US"),
+      dueDate: new Date().toLocaleDateString("en-US"),
+      notes: [],
+    },
+    {
+      id: uuidv4(),
+      title: "Get groceries",
+      status: "completed",
+      date: new Date().toLocaleDateString("en-US"),
+      dueDate: "",
+      notes: "",
+    },
+  ];
 
   // On first page load
   useEffect(() => {
@@ -20,39 +40,38 @@ function App() {
     const lS = window.localStorage;
 
     if (!lS.getItem("wilp_todo")) {
-      const mockData = [
-        {
-          id: uuidv4(),
-          title: "Clean your room",
-          status: "Not Started",
-          date: new Date().toLocaleDateString("en-US"),
-          dueDate: new Date().toLocaleDateString("en-US"),
-          notes: [],
-        },
-        {
-          id: uuidv4(),
-          title: "Get groceries",
-          status: "completed",
-          date: new Date().toLocaleDateString("en-US"),
-          dueDate: "",
-          notes: "",
-        },
-      ];
       lS.setItem("wilp_todo", JSON.stringify(mockData));
     } else {
       setTodos(JSON.parse(lS.getItem("wilp_todo")));
     }
+
+    if (!lS.getItem("wilp_theme")) {
+      lS.setItem("wilp_theme", "light");
+    } 
+    setTheme("light");
   }, []);
 
   return (
     <Router>
-      <div className="App">
+      <div className={`App ${theme}`}>
         <Navigation />
         <SearchBar />
         <div id="todo-container">
           <Routes>
             <Route path="/" element={<TodayPage data={todos} />} />
+            <Route path="/" element={<TodayPage data={todos} />} />
             <Route path="/completed" element={<CompletedPage data={todos} />} />
+            <Route
+              path="/settings"
+              element={
+                <Settings
+                  data={todos}
+                  mockData={mockData}
+                  theme={theme}
+                  setTheme={setTheme}
+                />
+              }
+            />
           </Routes>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,14 @@
 // Import pages
 import TodayPage from "./Today";
 import CompletedPage from "./Completed";
+<<<<<<< HEAD
 import Dashboard from "./Dashboard";
 
 //export pages
 export { Dashboard, TodayPage, CompletedPage };
+=======
+import Settings from "./Settings";
+
+//export pages
+export { TodayPage, CompletedPage, Settings };
+>>>>>>> df1247f (4 settings page (#5))
