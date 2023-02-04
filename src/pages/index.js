772745bea40c7fe359f6f75bace870e8b6a9bf0e
wilpola <wilpolaproduction@gmail.com>
2023-02-04 21:33:35 +0200

    Fixes rebase issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,23 +4,8 @@
 // Import pages
 import TodayPage from "./Today";
 import CompletedPage from "./Completed";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Dashboard from "./Dashboard";
-
-//export pages
-export { Dashboard, TodayPage, CompletedPage };
-=======
-import Settings from "./Settings";
-
-//export pages
-export { TodayPage, CompletedPage, Settings };
->>>>>>> df1247f (4 settings page (#5))
-=======
 import Dashboard from "./Dashboard";
 import Settings from "./Settings";
 
 //export pages
 export { TodayPage, CompletedPage, Settings, Dashboard };
-
->>>>>>> 5b9fa42 (Initializes dashboard page)
