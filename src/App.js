a34f23e6a1757cfbb45385b266bc67af4a2c5249
wilpola <wilpolaproduction@gmail.com>
2023-02-04 19:48:46 +0200

    Initilizes the settings page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import components
-import { Navigation } from "./components";
+import { Navigation, SearchBar } from "./components";
 
 // Import styles
 import "./App.scss";
 import { CompletedPage, TodayPage } from "./pages";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 // App component
 function App() {
@@ -25,15 +25,15 @@ function App() {
           id: uuidv4(),
           title: "Clean your room",
           status: "Not Started",
-          date: new Date().toLocaleDateString('en-US'),
-          dueDate: "2/3/2023",
+          date: new Date().toLocaleDateString("en-US"),
+          dueDate: new Date().toLocaleDateString("en-US"),
           notes: [],
         },
         {
           id: uuidv4(),
           title: "Get groceries",
           status: "completed",
-          date: new Date().toLocaleDateString('en-US'),
+          date: new Date().toLocaleDateString("en-US"),
           dueDate: "",
           notes: "",
         },
@@ -48,19 +48,13 @@ function App() {
     <Router>
       <div className="App">
         <Navigation />
+        <SearchBar />
         <div id="todo-container">
           <Routes>
-            <Route path="/today" element={<TodayPage data={todos} />} />
+            <Route path="/" element={<TodayPage data={todos} />} />
             <Route path="/completed" element={<CompletedPage data={todos} />} />
+            <Route path="/settings" element={<Settings data={todos} />} />
           </Routes>
-          {/* {todos.map((item, key) => {
-            return (
-              <div className="todoItem" key={key}>
-                <input type={"checkbox"} className="checkbox" />
-                <p>{item.title}</p>{" "}
-              </div>
-            );
-          })} */}
         </div>
       </div>
     </Router>
---
+++ src/pages/Settings/index.js
@@ -0,0 +1,24 @@
+// @author: wilpola
+// @desc: This file contains the settings page 
+
+
+// imports
+
+// Import styles
+
+// Settings component
+const Settings = (params) => {
+
+
+    // All rendered content
+    return (
+        <div className='settings-container'>
+            <div className="settings-area">
+
+            </div>
+        </div>
+    )
+} 
+
+// Export settings page
+export default Settings;
